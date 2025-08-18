e205cb6b10fb3cbcfdb2126c4d5eee4a86d9b2aa
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-08-18 21:41:49 +0200

    Here i am created a custom Tabbar

--- TabView10Lesson/ContentView.swift
+++ TabView10Lesson/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var page: String = "settings"
+    
+    var body: some View {
+        
+        ZStack(alignment: .bottom) {
+            TabView(selection: $page) {
+                Text("Home")
+                    .tag("home")
+                Text("Profile")
+                    .tag("profile")
+                SettingsPage(page: $page)
+                    .tag("settings")
+            }
+            HStack {
+                // 1
+                Button {
+                    page = "home"
+                } label: {
+                    HStack {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                }
+                
+                //2
+                Button {
+                    page = "profile"
+                } label: {
+                    HStack {
+                        Image(systemName: "house")
+                        Text("Second")
+                    }
+                }
+                
+                //3
+                Button {
+                    page = "settings"
+                } label: {
+                    HStack {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                }
+
+            }
+            .padding()
+            .background(Color.gray.opacity(0.5))
+            .padding(.bottom, 1)
+        }
+        
+    }
+}
+
+struct SettingsPage: View {
+    
+    @Binding var page: String
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Settings")
+            Button {
+                page = "home"
+            } label: {
+                Text("Save")
+            }
+
         }
-        .padding()
     }
 }
 
