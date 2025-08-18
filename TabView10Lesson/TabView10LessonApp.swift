79c3a8643f638c7696c382392a0bad819a124511
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-08-18 18:20:20 +0200

    Initial Commit

---
+++ TabView10Lesson/TabView10LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabView10LessonApp.swift
+//  TabView10Lesson
+//
+//  Created by Авазбек Надырбек уулу on 18.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TabView10LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
